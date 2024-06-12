a3926c12644e11f27930f16e261f8fccd9240233
Juri <juri.strumpflohner@gmail.com>
2024-06-12 15:34:10 +0200

    feat(react-query): add cool logic

--- packages/react-query/somelogic.ts
+++ packages/react-query/somelogic.ts
@@ -1,7 +1,7 @@
 import { queryCoreLogic } from '@juriorg/query-core';
 
 export function reactQueryLogic(): string {
-  const name = 'react-query-logic' + queryCoreLogic();
+  const name = 'react-query-logic: ' + queryCoreLogic();
   console.log(name);
   return name;
 }
